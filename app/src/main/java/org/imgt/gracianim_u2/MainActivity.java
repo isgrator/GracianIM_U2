b025dc1880673db66dac85c14ce232c2a9e5b8b4
Isabel <isgrator@gmail.com>
2017-10-12 19:01:20 +0200

    Terminado uso de FragmentTabHost (Apartado 2.10)

--- app/src/main/java/org/imgt/gracianim_u2/MainActivity.java
+++ app/src/main/java/org/imgt/gracianim_u2/MainActivity.java
@@ -1,13 +1,25 @@
 package org.imgt.gracianim_u2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    private FragmentTabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabHost = findViewById(android.R.id.tabhost);
+        tabHost.setup(this,
+                getSupportFragmentManager(),android.R.id.tabcontent);
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("Asteroides"),
+                Tab1.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("ConstraintLayout"),
+                Tab2.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Botón personal"),
+                Tab3.class, null);
     }
 }
