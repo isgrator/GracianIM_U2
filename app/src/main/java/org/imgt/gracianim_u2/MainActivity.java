93b262ef2d9fc116ba612a93a2534cdb0d14865a
Isabel <isgrator@gmail.com>
2017-10-12 20:34:54 +0200

    Terminado Traducción en Asteroides (Apartado 2.6)

--- app/src/main/java/org/imgt/gracianim_u2/MainActivity.java
+++ app/src/main/java/org/imgt/gracianim_u2/MainActivity.java
@@ -1,5 +1,6 @@
 package org.imgt.gracianim_u2;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
@@ -10,16 +11,22 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Resources res= getResources();
+        String titleTab1= res.getString(R.string.asteroides);
+        String titleTab2= res.getString(R.string.constraintlayout);
+        String titleTab3= res.getString(R.string.bot_n_personalizado);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tabHost = findViewById(android.R.id.tabhost);
         tabHost.setup(this,
                 getSupportFragmentManager(),android.R.id.tabcontent);
-        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("Asteroides"),
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator(titleTab1),
                 Tab1.class, null);
-        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("ConstraintLayout"),
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator(titleTab2),
                 Tab2.class, null);
-        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Botón personal"),
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator(titleTab3),
                 Tab3.class, null);
     }
 }
