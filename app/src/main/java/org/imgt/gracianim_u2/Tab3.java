744f9e2b0f1dd5a864c3116c389b1d6c24ce038c
Isabel <isgrator@gmail.com>
2017-10-13 22:53:51 +0200

    Arreglado problema con recurso dimens en Asteroides.

--- app/src/main/java/org/imgt/gracianim_u2/Tab3.java
+++ app/src/main/java/org/imgt/gracianim_u2/Tab3.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 
 public class Tab3 extends Fragment{
     @Override
@@ -15,5 +17,10 @@ public class Tab3 extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         return inflater.inflate(R.layout.tab3, container, false);
+    }
+
+
+    public void sePulsa(View view){
+        //Toast.makeText(getActivity(),"Pulsado",Toast.LENGTH_SHORT).show();
     }
 }
