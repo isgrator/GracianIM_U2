00f6bcabd26cfeed82451ef35302a5c85754d0a9
Isabel <isgrator@gmail.com>
2017-10-15 04:24:10 +0200

    Terminada Pestaña 4 con teclado de calculadora y conversor €-Ptas y Ptas-€ (Sin verificación de entradas)

--- app/src/main/java/org/imgt/gracianim_u2/Tab4.java
+++ app/src/main/java/org/imgt/gracianim_u2/Tab4.java
@@ -5,10 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Tab4 extends Fragment{
+
+    private EditText entrada;
+    private TextView salida;
+    private TextView moneda;
+    private Button bep;
+    private Button bpe;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -18,6 +26,44 @@ public class Tab4 extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.tab4, container, false);
+        View v =  inflater.inflate(R.layout.tab4, container, false);
+
+        entrada = (EditText) v.findViewById(R.id.entrada);
+        salida= (TextView)v.findViewById(R.id.salida);
+        moneda= (TextView)v.findViewById(R.id.moneda);
+        bep= (Button) v.findViewById(R.id.beuropesetas);
+
+
+
+        bep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                convEuroPeseta(view);
+                moneda.setText(R.string.ptas);
+            }
+        });
+
+        bpe= (Button) v.findViewById(R.id.bpesetaseuro);
+        bpe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                convPesetaEuro(view);
+                moneda.setText(R.string.euros);
+            }
+        });
+        return v;
+        //return inflater.inflate(R.layout.tab4, container, false);
     }
+
+    public void convEuroPeseta(View view){
+        salida.setText(String.valueOf(Float.parseFloat(entrada.getText().toString())*166.386));
+
+    }
+
+    public void convPesetaEuro(View view){
+        salida.setText(String.valueOf(Float.parseFloat(entrada.getText().toString())/166.386));
+
+    }
+
+
 }
